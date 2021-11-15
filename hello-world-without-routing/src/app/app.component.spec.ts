b6ec756dad4e797c220ddb17241a493ef21e4f5d
inpercima <inpercima@gmail.com>
2021-11-15 14:08:56 +0100

    Update angular-cli to 13.0.2

--- hello-world-without-routing/src/app/app.component.spec.ts
+++ hello-world-without-routing/src/app/app.component.spec.ts
@@ -30,7 +30,7 @@ describe('AppComponent', () => {
   it('should render toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('mat-toolbar').textContent).toContain('Hello world without routing');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('mat-toolbar')?.textContent).toContain('Hello world without routing');
   });
 });
